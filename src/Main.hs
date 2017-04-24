c07600a44be6efd69ba1df2049f37194512c55c2
Brokenprogrammer <brokenprogrammer@gmail.com>
2017-04-24 19:15:07 +0200

    Added boilerplate SDL code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Control.Concurrent (threadDelay)
+import Foreign.C.Types
+import SDL.Vect
+import qualified SDL
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (640, 480)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  SDL.initialize [SDL.InitVideo]
+
+  window <- SDL.createWindow "SDL Tutorial" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
+  SDL.showWindow window
+
+  screenSurface <- SDL.getWindowSurface window
+  let white = V4 maxBound maxBound maxBound maxBound
+  SDL.surfaceFillRect screenSurface Nothing white
+  SDL.updateWindowSurface window
+
+  threadDelay 2000000
+
+  SDL.destroyWindow window
+  SDL.quit
